44c41f664bebd24a23bee5ebed62e7843a6f22e2
georginm <georginmatheus@gmail.com>
2021-10-29 13:39:44 -0300

    feat: add api-doc with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
 import { router } from './routes';
+import swaggerFile from './swagger.json';
 
 const PORT = 3333;
 
 const app = express();
 
 app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 app.listen(PORT, () => {
