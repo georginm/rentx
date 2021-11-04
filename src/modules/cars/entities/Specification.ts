7d892324a4eccb54541d20da4d13f767f833c191
georginm <georginmatheus@gmail.com>
2021-11-04 14:54:23 -0300

    refactor: add typeorm decorators do entities

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,9 +1,18 @@
+import { Entity, PrimaryColumn, Column, CreateDateColumn } from 'typeorm';
 import { v4 as uuidV4 } from 'uuid';
 
+@Entity('specification')
 class Specification {
+  @PrimaryColumn()
   id?: string;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @CreateDateColumn()
   created_at?: Date;
 
   constructor() {
