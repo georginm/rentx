20d664a4824fde55450da53f6acac967ba949688
georginm <georginmatheus@gmail.com>
2021-11-08 16:03:17 -0300

    feat: add CreateUserController

---
+++ src/modules/accounts/useCases/createUser/CreateUserController.ts
@@ -0,0 +1,23 @@
+import { Response, Request } from 'express';
+import { container } from 'tsyringe';
+
+import { CreateUserUseCase } from './CreateUserUseCase';
+
+class CreateUserController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, email, password, driverLicense } = request.body;
+
+    const createUserUseCase = container.resolve(CreateUserUseCase);
+
+    await createUserUseCase.execute({
+      name,
+      email,
+      password,
+      driverLicense,
+    });
+
+    return response.status(201).send();
+  }
+}
+
+export { CreateUserController };
