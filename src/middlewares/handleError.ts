27576dd99c06e93e68c89a5c4a2444eda66788d9
georginm <georginmatheus@gmail.com>
2021-11-11 14:11:30 -0300

    feat: add middleware to handle error

---
+++ src/middlewares/handleError.ts
@@ -0,0 +1,18 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { AppError } from '../errors/AppError';
+
+export const handleError =
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: `Internal server error - ${err.message}`,
+    });
+  };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
 import './database';
 import './shared/container';
 
+import { handleError } from './middlewares/handleError';
 import { router } from './routes';
 import swaggerFile from './swagger.json';
 
@@ -14,6 +16,7 @@ const app = express();
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
+app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
