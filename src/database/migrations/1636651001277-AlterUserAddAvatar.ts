ca00a44bd88c1acf0fcaaf8773dee69065980bea
georginm <georginmatheus@gmail.com>
2021-11-11 16:55:19 -0300

    feat: add avatar to users

---
+++ src/database/migrations/1636651001277-AlterUserAddAvatar.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner, TableColumn } from 'typeorm';
+
+export class AlterUserAddAvatar1636651001277 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      'users',
+      new TableColumn({
+        name: 'avatar_url',
+        type: 'varchar',
+        isNullable: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn('users', 'avatar_url');
+  }
+}
