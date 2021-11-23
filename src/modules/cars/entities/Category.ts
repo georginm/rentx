932f9fbf6355b5c1a523982a0bc35f917bd7ef7f
georginm <georginmatheus@gmail.com>
2021-11-23 14:02:48 -0300

    refactor: change created_at to camelCase: createdAt

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -12,8 +12,8 @@ class Category {
   @Column()
   description: string;
 
-  @CreateDateColumn()
-  created_at?: Date;
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt?: Date;
 
   constructor() {
     if (!this.id) this.id = uuidV4();
