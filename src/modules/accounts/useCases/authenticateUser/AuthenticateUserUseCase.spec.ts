c3d26eff865bf3a53424c9f1b27dcccbb627d916
georginm <georginmatheus@gmail.com>
2021-11-23 14:22:08 -0300

    test: should be able to authenticate an user

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -17,4 +17,21 @@ describe('Authenticate User', () => {
     createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
   });
 
+  it('Should be able to authenticate an user', async () => {
+    const user: ICreateUserDTO = {
+      driverLicense: '000123',
+      email: 'test@mail.com',
+      name: 'Test unitary',
+      password: '123456',
+    };
+
+    await createUserUseCase.execute(user);
+
+    const result = await authenticateUserUseCase.execute({
+      email: user.email,
+      password: user.password,
+    });
+
+    expect(result).toHaveProperty('token');
+  });
 });
