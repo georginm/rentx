83a6d98175d72f9e3ecf89a3a53b97661a4de1ce
georginm <georginmatheus@gmail.com>
2022-03-01 09:24:37 -0300

    test: add CreateRentalUseCase test

---
+++ src/modules/rentals/useCases/CreateRentalUseCase.spec.ts
@@ -0,0 +1,20 @@
+import { RentalsRepositoryInMemory } from '../repositories/in-memory/RentalsRepositoryInMemory';
+import { CreateRentalUseCase } from './CreateRentalUseCase';
+
+let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+
+describe('Create Rental', () => {
+  beforeEach(() => {
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
+  });
+
+  it('should be able to create a new rental', async () => {
+    await createRentalUseCase.execute({
+      userId: '12345',
+      carId: '12121212',
+      expectedReturnDate: new Date(),
+    });
+  });
+});
