3c1482d29ea9d8b90a6c6f80ff31d1ff2110360c
georginm <georginmatheus@gmail.com>
2022-03-18 19:33:53 -0300

    chore: add createRental routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { authenticateRoutes } from './authenticate.routes';
 import { carsRoute } from './cars.routes';
 import { categoriesRoutes } from './categories.routes';
+import { rentalRoutes } from './rental.routes';
 import { specificationRoutes } from './specification.routes';
 import { usersRoutes } from './users.routes';
 
@@ -12,6 +13,7 @@ router.use('/categories', categoriesRoutes);
 router.use('/specifications', specificationRoutes);
 router.use('/users', usersRoutes);
 router.use('/cars', carsRoute);
+router.use('/rentals', rentalRoutes);
 router.use(authenticateRoutes);
 
 export { router };
