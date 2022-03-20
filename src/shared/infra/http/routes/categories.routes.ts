d52ba91b6d08ff220b7abef01d2b8b4fffcddc20
georginm <georginmatheus@gmail.com>
2022-03-20 18:24:29 -0300

    fix: remove ensureAuthenticated from categories routes

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -18,8 +18,6 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoryController = new ListCategoriesController();
 
-categoriesRoutes.use(ensureAuthenticated);
-
 categoriesRoutes.post(
   '/',
   ensureAuthenticated,
