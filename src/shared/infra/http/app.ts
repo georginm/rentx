378a1bf699123ed9b399642231efe479f4ad90e1
georginm <georginmatheus@gmail.com>
2022-03-20 17:00:44 -0300

    chore: separate app and server

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
+
+import createConnection from '@shared/infra/typeorm';
+import '@shared/container';
+
+import swaggerFile from '../../../swagger.json';
+import handleError from './middlewares/handleError';
+import { router } from './routes';
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use(router);
+app.use(handleError);
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,23 +1,6 @@
-import express from 'express';
-import 'express-async-errors';
-import swaggerUi from 'swagger-ui-express';
-
-import createConnection from '@shared/infra/typeorm';
-import '@shared/container';
-
-import swaggerFile from '../../../swagger.json';
-import handleError from './middlewares/handleError';
-import { router } from './routes';
+import { app } from './app';
 
 const PORT = 3333;
-
-createConnection();
-const app = express();
-
-app.use(express.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-app.use(router);
-app.use(handleError);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
