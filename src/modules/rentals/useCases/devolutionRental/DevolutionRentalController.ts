fa5fb465b08101970be477735de94d9f4b291291
georginm <georginmatheus@gmail.com>
2022-03-20 22:44:21 -0300

    chore: add devolution rental controller

---
+++ src/modules/rentals/useCases/devolutionRental/DevolutionRentalController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import { DevolutionRentalUseCase } from './DevolutionRentalUseCase';
+
+class DevolutionRentalController {
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { id: userId } = request.user;
+    const { id } = request.params;
+
+    const devolutionRentalUseCase = container.resolve(DevolutionRentalUseCase);
+
+    const rental = await devolutionRentalUseCase.execute({ id, userId });
+
+    return response.status(200).json(rental);
+  }
+}
+
+export { DevolutionRentalController };
