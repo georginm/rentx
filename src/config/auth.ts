dd395218b7716fd2d7f3398f513ffc41f42f9876
georginm <georginmatheus@gmail.com>
2022-04-07 11:48:01 -0300

    chore: add authentication settings

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secretToken: 'be0df4a8dd91d3d0946aa9205f8320f6',
+  expiresInToken: '1d',
+  secretRefreshToken: 'be0df4a8dd91d3d0946aa9205f8320f6',
+  expiresInRefreshToken: '30d',
+  expiresRefreshTokenDays: 30,
+};
