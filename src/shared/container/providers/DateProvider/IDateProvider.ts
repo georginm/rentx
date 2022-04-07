90ecfc042d261f5a78c4a8927e9bc7ff76d02582
georginm <georginmatheus@gmail.com>
2022-04-07 11:46:22 -0300

    chore: add days to date

--- src/shared/container/providers/DateProvider/IDateProvider.ts
+++ src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -3,6 +3,7 @@ interface IDateProvider {
   convertToUTC(date: Date): string;
   dateNow(): Date;
   compareInDays(startDate: Date, endDate: Date): number;
+  addDays(days: number): Date;
 }
 
 export { IDateProvider };
