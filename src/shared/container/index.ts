478e2d6572e8d55f5cd7c2d6e7ae54e3211c3447
georginm <georginmatheus@gmail.com>
2022-04-28 14:29:34 -0300

    feat: create MailProvider

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,6 +1,7 @@
 import { container } from 'tsyringe';
 
 import '@shared/container/providers/DateProvider';
+import '@shared/container/providers/MailProvider';
 
 import { UsersRepository } from '@modules/accounts/infra/typeorm/repositories/UsersRepository';
 import { UsersTokensRepository } from '@modules/accounts/infra/typeorm/repositories/UsersTokensRepository';
