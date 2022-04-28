351c309e1f59850789cf31b80667316d890e8f8b
georginm <georginmatheus@gmail.com>
2022-04-28 17:24:42 -0300

    test: add test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,13 @@ import { compilerOptions } from './tsconfig.json';
 export default {
   bail: true,
   clearMocks: true,
+
+  collectCoverage: true,
+  collectCoverageFrom: ['<rootDir>/src/modules/**/useCases/**/*.ts'],
+  coverageDirectory: 'coverage',
   coverageProvider: 'v8',
+
+  coverageReporters: ['text-summary', 'lcov'],
 
   moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
     prefix: '<rootDir>/src',
