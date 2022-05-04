d3b1a9737a72c485bb133c21e6d052287aa4dcfa
georginm <georginmatheus@gmail.com>
2022-05-04 16:41:26 -0300

    refactor: use PORT environment

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,7 +1,5 @@
 import { app } from './app';
 
-const PORT = 3333;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
